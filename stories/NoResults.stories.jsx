b8e002986466eb57ad0fcf8638bf33489865b876
cefoFintecimal <eduardo@fintecimal.com>
2021-04-17 16:17:08 -0500

    NoResults Component

---
+++ stories/NoResults.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import NoResults from '../app/components/NoResults';
+
+export const Main = () => (
+  <NoResults />
+);
+
+export default {
+  title: 'NoResults',
+  component: Main,
+};
