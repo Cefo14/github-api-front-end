8d09ce33799dff375f9391854cd64c33ec2846e6
cefoFintecimal <eduardo@fintecimal.com>
2021-04-17 23:52:53 -0500

    add repositories e2e test

--- cypress/integration/repositories.e2e.spec.js
+++ cypress/integration/repositories.e2e.spec.js
@@ -1,1 +1,29 @@
-// TODO...
+/// <reference types="Cypress" />
+/// <reference types="cypress-react-selector" />
+
+describe('Repositories e2e', () => {
+  describe('when is ok', () => {
+    it('should visit users route and to be ok', () => {
+      cy.visit('/');
+
+      cy.waitForReact();
+
+      cy
+        .get('*[data-testid="CardRoute"]')
+        .eq(1)
+        .click();
+
+      cy.waitForReact();
+
+      cy
+        .get('*[data-testid="Search-Input"]')
+        .type('a');
+
+      cy.waitForReact();
+
+      cy
+        .get('*[data-testid="GithubCard"]')
+        .should('have.length.gte', 1)
+    });
+  });
+});
